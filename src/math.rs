df6d3623a615c8daf315fc7ae05d4d7e845a6b41
bit doer <bitdoer@gmail.com>
2021-03-09 03:52:36 -0600

    fixed (hopefully) factor_root_mod bug

--- src/math.rs
+++ src/math.rs
@@ -178,32 +178,41 @@ impl Polynomial {
         }
     }
 
-    /// If `a` is a root of `self`, this returns the result of factoring
-    /// `x - a` out of `self`, if everything is considered a polynomial with
-    /// coefficients modulo `div`. Otherwise returns `None`.
+    /// If `a` is a root of `self` and if `p` is a prime, this returns the
+    /// result of factoring `x - a` out of `self`, if everything is considered
+    /// a polynomial with coefficients modulo `p`. Otherwise returns `None`.
     ///
     /// # Examples
     /// ```
     /// use polynomint::{Polynomial, poly};
     ///
     /// let poly = poly![12, -8, 1]; // x^2 - 8x + 12 = (x - 2)(x - 6)
+    ///                              // = x^2 + x (mod 3) = x(x - 2) or x(x + 1) mod 3
+    ///                              // = x^2 + 2x + 2 = (x - 2)(x + 4) or (x - 1)(x + 3) mod 5
     /// assert_eq!(poly.factor_root_mod(2, 3), Some(poly![0, 1]));
     /// assert_eq!(poly.factor_root_mod(0, 3), Some(poly![1, 1]));
     /// assert_eq!(poly.factor_root_mod(1, 3), None);
+    /// assert_eq!(poly.factor_root_mod(2, 5), Some(poly![4, 1]));
+    /// assert_eq!(poly.factor_root_mod(1, 5), Some(poly![3, 1]));
+    /// assert_eq!(poly.factor_root_mod(0, 5), None);
+    ///
+    /// let poly2 = poly![1, 0, 1]; // x^2 + 1 = (x + 1)^2 mod 2
+    ///
+    /// assert_eq!(poly2.factor_root_mod(1, 2), Some(poly![1, 1]));
+    /// assert_eq!(poly2.factor_root_mod(0, 2), None);
     /// ```
-    pub fn factor_root_mod(&self, a: isize, div: isize) -> Option<Self> {
-        // if not a root, we're done
-        if !self.has_root_mod(a, div) {
+    pub fn factor_root_mod(&self, a: isize, p: isize) -> Option<Self> {
+        // if not a root or p isn't prime, we're done
+        if !self.has_root_mod(a, p) || !Self::is_prime(p as usize) {
             None
         // if polynomial is zero, everything's a root, and the factoring gives zero again
         } else if self.is_zero() {
             Some(Self::zero())
-        // if zero is a root, then we can just skip the constant, reduce mod n, and be done
+        // if zero is a root, then we can just skip the constant, reduce mod div, and be done
         } else if a == 0 {
             let mut output = Self {
-                coeffs: self.iter().skip(1).copied().collect(),
+                coeffs: self.rem_euclid(p).iter().skip(1).copied().collect(),
             };
-            output = output.rem_euclid(div);
             output.reduce();
             Some(output)
         // otherwise, we know that the last coefficient b[0] of the output
@@ -213,13 +222,43 @@ impl Polynomial {
             let mut coeffs = Vec::new();
             // keep an accumulator,
             let mut acc = 0;
-            for &coeff in self.iter().take(self.degree() as usize) {
+            for &coeff in self.rem_euclid(p).iter().take(self.degree() as usize) {
                 // and at each step, subtract c[n] and divide by a
                 acc -= coeff;
-                acc /= a;
-                coeffs.push(acc.rem_euclid(div));
+                acc *= Self::inv_mod_p(a.rem_euclid(p), p);
+                coeffs.push(acc.rem_euclid(p));
             }
             Some(Self { coeffs })
         }
+    }
+
+    fn is_prime(p: usize) -> bool {
+        if p == 2 || p == 3 {
+            true
+        } else if p == 1 || p % 2 == 0 || p % 3 == 0 {
+            false
+        } else {
+            // we need only search for prime factors up to the sqrt of n;
+            // every prime past 3 is either 1 or 5 mod 6, so we can quickly
+            // reduce our search space to a size of approx sqrt(n)/3
+            for i in (5..((p as f64).sqrt().floor() as usize)).filter(|&x| x % 6 == 1 || x % 6 == 5)
+            {
+                if p % i == 0 {
+                    return false;
+                }
+            }
+            true
+        }
+    }
+
+    fn inv_mod_p(a: isize, p: isize) -> isize {
+        let mut r_pair = (a, p);
+        let mut s_pair = (1, 0);
+        while r_pair.1 != 0 {
+            let quot = r_pair.0 / r_pair.1;
+            r_pair = (r_pair.1, r_pair.0 - quot * r_pair.1);
+            s_pair = (s_pair.1, s_pair.0 - quot * s_pair.1);
+        }
+        s_pair.0.rem_euclid(p)
     }
 }
